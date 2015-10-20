eca0b7836757033a7ae6269ef64d19b7656b24a8
samw <samw@ad164d9f-72a4-b243-94de-9da5bd454896>
2015-10-20 21:04:31 +0000

    vs2015 project compiles OK
    git-svn-id: http://sam2kfast:8000/svn/src/Libs/ccLib/trunk@714 ad164d9f-72a4-b243-94de-9da5bd454896

--- Src/frSelectEncoding.cpp
+++ Src/frSelectEncoding.cpp
@@ -81,7 +81,7 @@ bool frSelectEncoding::FillControls( const QString& path, int current_encoding_i
     if ( file_info.isSymLink() || !file_info.isFile() )
         return false;
 
-    BOM::type               mCurrentBOM = GetBom( path );
+    mCurrentBOM = GetBom( path );
 
     if ( ui.cbTextPreview->checkState() != Qt::Checked )
         return false;
