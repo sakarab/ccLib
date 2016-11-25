964ae3e079ded0553ba25088a84e487dc2a2cdb2
Sam <sam@dummy.com>
2016-11-26 01:11:24 +0200

    use boost::filesystem in LibLoader

--- Src/LibLoader.cpp
+++ Src/LibLoader.cpp
@@ -7,30 +7,33 @@ namespace ccwin
     /************************************************************
     ********    LibLoader
     ************************************************************/
-    LibLoader::LibLoader( const std::wstring& dll_name )
-        : mDllName( dll_name ), mDll( 0 )
+    LibLoader::LibLoader( const boost::filesystem::path& dll_name )
+        : mDllName( dll_name ), mDll()
     {
+        LoadLib( dll_name );
     }
     
-    LibLoader::LibLoader( const std::wstring& dll_name, HMODULE parent_module )
-        : mDllName( dll_name ), mDll( parent_module )
-    {
-    }
+    //LibLoader::LibLoader( const boost::filesystem::path& dll_name, HMODULE parent_module )
+    //    : mDllName(), mDll()
+    //{
+    //}
     
     LibLoader::~LibLoader()
     {
+        if ( mDll )
+            FreeLibrary( mDll );
     }
 
-    void LibLoader::LoadLib( const std::wstring & dll_name )
+    void LibLoader::LoadLib( const boost::filesystem::path& dll_name )
     {
         mDll = LoadLibrary( dll_name.c_str() );
         if ( mDll == 0 )
-            throw cclib::BaseException( boost::str( boost::format( "%1%: library not found." ) % NarrowStringStrict( dll_name ) ) );
+            throw cclib::BaseException( boost::str( boost::format( "%1%: library not found." ) % dll_name ) );
     }
 
     std::string LibLoader::EntryPointNotFound( const char *function_name )
     {
-        return boost::str( boost::format( "%1%: entry point not found." ) % function_name );
+        return boost::str( boost::format( "%1%: entry point not found in %2%." ) % function_name % mDllName.filename() );
     }
 
 }
--- Src/LibLoader.h
+++ Src/LibLoader.h
@@ -6,6 +6,7 @@
 #include <Windows.h>
 #include "predef_cc.h"
 #include <boost\format.hpp>
+#include <boost\filesystem.hpp>
 #include "smException.h"
 #include "cpp_string.h"
 
@@ -18,18 +19,18 @@ namespace ccwin
     class LibLoader
     {
     private:
-        HMODULE         mDll;
-        std::wstring    mDllName;
+        HMODULE                     mDll;
+        boost::filesystem::path     mDllName;
 
-        void LoadLib( const std::wstring& dll_name );
+        void LoadLib( const boost::filesystem::path& dll_name );
         std::string EntryPointNotFound( const char *function_name );
 
         // noncopyable
         LibLoader( const LibLoader& src ) CC_EQ_DELETE;
         LibLoader& operator = ( const LibLoader& src ) CC_EQ_DELETE;
     public:
-        LibLoader( const std::wstring& dll_name );
-        LibLoader( const std::wstring& dll_name, HMODULE parent_module );
+        LibLoader( const boost::filesystem::path& dll_name );
+        //LibLoader( const boost::filesystem::path& dll_name, HMODULE parent_module );
         ~LibLoader();
 
         template <class T> T GetFuncAddress( const char *function_name )
