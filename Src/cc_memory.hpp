1306cace3181faabbecf36553d364c02e5eff88d
Sam <sam@dummy.com>
2017-10-02 00:30:55 +0300

    fix: no include in namespaces

--- Src/cc_memory.hpp
+++ Src/cc_memory.hpp
@@ -26,16 +26,20 @@
 
 #include "predef_cc.h"
 
+#if defined (CC_HAVE_SHARED_PTR)
+    #include <memory>
+#else
+    #include <boost/smart_ptr.hpp>
+#endif
+
 namespace cclib
 {
     template <class TO, class FROM> TO size_cast( FROM value )          { return static_cast<TO>(value); }
 
 #if defined (CC_HAVE_SHARED_PTR)
-    #include <memory>
     using std::shared_ptr;
     using std::make_shared;
 #else
-    #include <boost/smart_ptr.hpp>
     using boost::shared_ptr;
     using boost::make_shared;
 #endif
