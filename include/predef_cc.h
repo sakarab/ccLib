90d58ee2860d64e80e0ddc9b1f3d1065bc158f04
saml <saml@dummy.com>
2018-07-28 21:56:57 +0300

    [Linux] fix predef_cc for gcc 5.4

--- include/predef_cc.h
+++ include/predef_cc.h
@@ -57,14 +57,20 @@
 #elif defined (BOOST_COMP_GNUC_AVAILABLE)
 
     #define CC_HAVE_COPY_DELETE
-    #define CC_HAVE_ATOMIC
+    //#define CC_HAVE_COPY_DEFAULT
+    //#define CC_HAVE_ATOMIC
     #define CC_HAVE_INLINE_FUNCTORS
     #define CC_HAVE_OVERRIDE
+    #define CC_HAVE_ENUM_CLASS
+    #define CC_HAVE_RANGE_FOR
+    #define CC_HAVE_THREAD
+    #define CC_HAVE_VARIADIC_TEMPLATES
+    //#define CC_HAVE_MAKE_UNIQUE
+
     #define CC_HAVE_MOVE_CTOR
     #define CC_HAVE_NULL_PTR
     #define CC_HAVE_SHARED_PTR
     #define CC_HAVE_ARRAY
-    #define CC_HAVE_VARIADIC_TEMPLATES
 
 #endif
 
