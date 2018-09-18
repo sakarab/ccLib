c73fc9b99956133d2bcc4608ac5e36c8a83a5924
Sam <sam@dummy.com>
2018-09-18 09:32:49 +0300

    [vs2017] ccwx library development (2)

--- Src/wxApp.cpp
+++ Src/wxApp.cpp
@@ -8,10 +8,34 @@ namespace ccwx
     //=======================================================================
     int Application::OnRun()
     {
-        return 0;
+        int     result;
+
+        try
+        {
+            result = wxApp::OnRun();
+        }
+        catch ( ... )
+        {
+            result = 1;
+        }
+        return result;
     }
 
     void Application::HandleEvent( wxEvtHandler * handler, wxEventFunction func, wxEvent & event ) const
     {
+        try
+        {
+            wxApp::HandleEvent( handler, func, event );
+        }
+        catch ( const std::exception& e )
+        {
+            ::wxMessageBox( wxString( e.what(), wxConvLocal ), wxT( "Error" ) );
+            throw;
+        }
+        catch ( ... )
+        {
+            ::wxMessageBox( wxT( "Unknown Error" ), wxT( "Error" ) );
+            throw;
+        }
     }
 }
