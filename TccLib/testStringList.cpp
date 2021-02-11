7f578c7174cfcf0c1faf9c3f7ded410892091fd2
Sam <sam@dummy.com>
2021-02-11 12:01:25 +0200

    [cclib] add cpp_string_list.h (heavily modified cpp_StringList.h)

--- TccLib/testStringList.cpp
+++ TccLib/testStringList.cpp
@@ -1,45 +1,45 @@
 #include "stdafx.h"
 
 #include <vector>
+#include <list>
 #include <string>
-#include <cpp_StringList.h>
+#include <cpp_string_list.h>
 
 #include "cclib_tests.h"
 
 namespace
 {
-    using StringList = std::vector<std::string>;
+    using StringViewList = std::vector<std::string_view>;
+    using WStringViewList = std::vector<std::wstring_view>;
+    using StringVector = std::vector<std::string>;
+    using StringList = std::list<std::string>;
     using CharBuffer = std::vector<char>;
-
-    StringList CreateList()
-    {
-        return StringList { "", "", "" };
-    }
-
-    CharBuffer CreateBuffer()
-    {
-        return CharBuffer { 'a', 'b', 'c' };
-    }
-
 }
-
 
 void testStringList( CRichEditCtrl& logger )
 {
-    StringList      original = CreateList();
-    CharBuffer      buffer = CreateBuffer();
+    StringVector    original = { "", "", "" };
+    StringList      list = { "", "", "" };
+    CharBuffer      buffer   = { 'a', 'b', 'c' };
 
     // string from vector<string>
-    std::string     sstr = cclib::Text<char>( original );
+    std::string     sstr = cclib::ToString( original );
+
+    sstr = cclib::ToString( original, 'p' );
 
     // string from vector<string>::iterator
-    std::string     sstr_2 = cclib::StringFromListIT( original.begin(), original.end() );
+    sstr = cclib::ToString( original.begin(), original.end() );
+    sstr = cclib::ToString( original.cbegin(), original.cend() );
+    sstr = cclib::ToString( original.begin(), original.end(), 'p' );
+    sstr = cclib::ToString( original.cbegin(), original.cend(), 'p' );
 
     // vector from any CHAR iterator
-    StringList      slist_1 = cclib::ListFromCharIT( sstr.cbegin(), sstr.cend() );
-    StringList      slist_2 = cclib::ListFromCharIT( buffer.cbegin(), buffer.cend() );
+    StringVector    vvec = cclib::ToList<StringVector>( sstr.begin(), sstr.end() );
+
+    vvec = cclib::ToList<StringVector>( sstr.begin(), sstr.end(), 'p' );
+    vvec = cclib::ToList<StringVector>( sstr.cbegin(), sstr.cend() );
+    vvec = cclib::ToList<StringVector>( sstr.cbegin(), sstr.cend(), 'p' );
 
     // vector from string
-    StringList      slist_3 = cclib::Text<char>( sstr );
-
+    vvec = cclib::ToList<StringVector>( sstr );
 }
---
+++ include/cpp_string_list.h
@@ -0,0 +1,176 @@
+//***************************************************************************
+// ccLib - Frequently used program snippets
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+// Please read the "License.txt" for more copyright and license
+// information.
+//***************************************************************************
+
+#ifndef CPP_STRING_LIST_H
+#define CPP_STRING_LIST_H
+
+#include <string>
+#include <vector>
+#include <cpp_string.h>
+
+namespace cclib
+{
+    template <class CH> struct type_helper
+    {
+        typedef std::basic_string<CH, std::char_traits<CH>, std::allocator<CH>>     string_type;
+        typedef std::basic_string_view<CH, std::char_traits<CH>>                    string_view_type;
+    };
+
+#pragma region ToString
+    template <class IT>
+    typename std::iterator_traits<IT>::value_type
+    ToString( IT begin, IT end )
+    {
+        using STR = typename std::iterator_traits<IT>::value_type;
+        using CH = typename STR::value_type;
+
+        std::vector<CH>     buffer;
+
+        while ( begin != end )
+        {
+            buffer.insert( buffer.end(), begin->begin(), begin->end() );
+            buffer.push_back( CharConstant<CH>::cr );
+            buffer.push_back( CharConstant<CH>::lf );
+            ++begin;
+        }
+        if ( buffer.empty() )
+            return STR();
+        return STR( &buffer.front(), buffer.size() );
+    }
+
+    template <class IT>
+    typename std::iterator_traits<IT>::value_type
+    ToString( IT begin, IT end, typename std::iterator_traits<IT>::value_type::value_type ch )
+    {
+        using STR = typename std::iterator_traits<IT>::value_type;
+        using CH = typename STR::value_type;
+
+        std::vector<CH>     buffer;
+
+        while ( begin != end )
+        {
+            buffer.insert( buffer.end(), begin->begin(), begin->end() );
+            buffer.push_back( ch );
+            ++begin;
+        }
+        if ( buffer.empty() )
+            return STR();
+        return STR( &buffer.front(), buffer.size() - 1 );
+    }
+
+    // string from vector<string>
+    template <class CH>
+    typename CH::value_type
+    ToString( const CH& list )
+    {
+        return ToString( list.cbegin(), list.cend() );
+    }
+
+    template <class CH>
+    typename CH::value_type
+    ToString( const CH& list, typename CH::value_type::value_type delimiter )
+    {
+        return ToString( list.cbegin(), list.cend(), delimiter );
+    }
+
+#pragma endregion
+
+#pragma region ToList from char iterators
+    template <typename LIST, typename IT>
+    LIST ToList( IT begin, IT end )
+    {
+        using CH = typename LIST::value_type::value_type;
+        using STR = typename LIST::value_type;
+
+        LIST        result;
+
+        while ( begin != end )
+        {
+            IT      start = begin;
+            CH      ch = *begin;
+
+            while ( ch != CharConstant<CH>::cr && ch != CharConstant<CH>::lf )
+            {
+                ++begin;
+                if ( begin == end )
+                    break;
+                ch = *begin;
+            }
+            result.push_back( STR( start, begin ) );
+            AdvanceOverCRLF( begin, end, ch );
+        }
+        return result;
+    }
+
+    template <typename LIST, typename IT>
+    LIST ToList( IT begin, IT end, typename LIST::value_type::value_type delimiter )
+    {
+        using STR = typename LIST::value_type;
+
+        LIST        result;
+
+        while ( begin != end )
+        {
+            IT      start = begin;
+
+            while ( *begin != delimiter )
+            {
+                ++begin;
+                if ( begin == end )
+                    break;
+            }
+            result.push_back( STR( start, begin ) );
+            if ( begin != end )
+                ++begin;
+        }
+        return result;
+    }
+
+    template <typename LIST>
+    LIST ToList( const typename type_helper<typename LIST::value_type::value_type>::string_type& sstr )
+    {
+        return ToList<LIST>( sstr.begin(), sstr.end() );
+    }
+
+    template <class LIST>
+    LIST ToList( const typename type_helper<typename LIST::value_type::value_type>::string_type& sstr,
+                    typename LIST::value_type::value_type delimiter )
+    {
+        return ToList<LIST>( sstr.begin(), sstr.end(), delimiter );
+    }
+
+    template <class LIST>
+    LIST ToList( const typename type_helper<typename LIST::value_type::value_type>::string_view_type& sstr )
+    {
+        return ToList<LIST>( sstr.begin(), sstr.end() );
+    }
+
+    template <class LIST>
+    LIST ToList( const typename type_helper<typename LIST::value_type::value_type>::string_view_type& sstr,
+                    typename LIST::value_type::value_type delimiter )
+    {
+        return ToList<LIST>( sstr.begin(), sstr.end(), delimiter );
+    }
+#pragma endregion
+
+}
+
+#endif
